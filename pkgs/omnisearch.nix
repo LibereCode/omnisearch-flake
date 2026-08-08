e8448b5c3dcce66ae2f973686eacd27e9e1bd839
LibereCode <libere-code@posteo.org>
2026-08-08 12:11:38 +0200

    feat(ok): first succesfull build
    Still missing systemd, working on it...

--- pkgs/omnisearch.nix
+++ pkgs/omnisearch.nix
@@ -3,56 +3,72 @@
   libxml2,
   curl,
   openssl,
-  fetchgit,
+  git,
 
   #TEST: temp
   lib,
 }:
 let
-  beaker = stdenv.mkDerivation {
+  repoURLBase = "https://git.bwaaa.monster";
+
+  beaker = stdenv.mkDerivation rec {
     pname = "beaker";
-    version = "git";
-    src = fetchgit {
-      url = "https://git.bwaa.monster/beaker";
-      rev = "9c68a8ae6fb32f8a1660da392b9985a4ab3e7cb4"; # TEST:
-      hash = lib.fakeHash;
-      buildPhase = /* sh */ "make";
-      installPhase = /* sh */ "make install";
-      ## from old commit
-      makeFlags = [
-        "INSTALL_PREFIX=$(out)/"
-        "LDCONFIG=true"
-      ];
+    version = "360d6271e1a20d128430e52637d5d35f4c706ca5"; # "360d627";
+
+    src = fetchGit {
+      url = "${repoURLBase}/${pname}";
+      rev = version;
     };
+
+    buildPhase = /* sh */ ''
+      make
+    '';
+    installPhase = /* sh */ ''
+      make INSTALL_PREFIX="$out/" install
+    '';
+    makeFlags = [
+      "INSTALL_PREFIX=$(out)/"
+      "LDCONFIG=true"
+    ];
   };
 in
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "omnisearch";
-  version = "git";
-  src = fetchgit {
-    url = "https://git.bwaa.monster/omnisearch";
-    rev = "HEAD";
-    hash = lib.fakeHash;
+  version = "499bb9b1268cd422619efdc46889960425462aae"; # "9c68a8a";
+
+  src = fetchGit {
+    url = "${repoURLBase}/${pname}";
+    rev = version;
+    # rev = "9c68a8ae6fb32f8a1660da392b9985a4ab3e7cb4";
   };
 
   buildInputs = [
-    libxml2.dev
-    curl.dev
+    libxml2
+    curl
     openssl
     beaker
+    git
   ];
-
-  #XXX: bad?
   preBuild = ''
     makeFlagsArray+=(
-      "PREFIX=$out"
-      "CFLAGS=-Wall -Wextra -O2 -Isrc -I${libxml2.dev}/include/libxml2"
-      "LIBS=-lbeaker -lcurl -lxml2 -lpthread -lm -lssl -lcrypto"
+      GIT_HASH="$(git -C $src rev-parse --short ${version})"
+      GIT_DATE="$(git -C $src log -1 --format='%ad' --date='format:%y.%m.%d')"
+      GIT_BRANCH="$(git -C $src rev-parse --abbrev-ref ${version})"
+      GIT_REMOTE="$(git -C $src remote get-url origin)"
     )
   '';
+  buildPhase = # sh
+    ''
+      #NOTE: I don't understand why I cant put everything in makeFlagArray...
+      make \
+        PREFIX="$out" \
+        CFLAGS="-Wall -Wextra -O2 -Isrc -I${libxml2.dev}/include/libxml2" \
+        LIBS="-lbeaker -lcurl -lxml2 -lpthread -lm -lssl -lcrypto" \
+        ''${makeFlagsArray[@]}
 
-  buildPhase = "make";
-  # installPhase = "make install-init";
+      # make install-systemd #TODO: HOLY SHIT WORKS WITHOUT THIS!
+    '';
+
   installPhase = ''
     mkdir -p $out/bin $out/share/omnisearch
     install -Dm755 bin/omnisearch $out/bin/omnisearch
@@ -62,5 +78,6 @@ stdenv.mkDerivation {
   meta = {
     description = "Lightweight metasearch engine in C";
     platforms = lib.platforms.linux;
+    license = [ ../omnisearch.LICENSE ]; # TEST:
   };
 }
