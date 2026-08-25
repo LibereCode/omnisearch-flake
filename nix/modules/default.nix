1f0bae85e6064ecc821a083670b019a4404e123f
LibereCode <libere-code@posteo.org>
2026-08-25 06:24:53 +0200

    fix: user + group + systemd

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -11,6 +11,7 @@
       cfg = config.services.omnisearch;
       # pkg = pkgs.omnisearch;
       pkg = self.packages.${pkgs.stdenv.hostPlatform.system}.omnisearch;
+      default_user = "omnisearch";
       inherit (lib)
         literalMD
         mkOption
@@ -39,6 +40,37 @@
           '';
         };
 
+        dataDir = mkOption {
+          type = types.path;
+          default = "/var/lib/" + cfg.user;
+          example = "/tmp/weird/place";
+          description = ''
+            The working and home directory of the user.
+          '';
+        };
+
+        user = mkOption {
+          type = types.str;
+          default = default_user;
+          example = "LimitedQuery";
+          description = ''
+            The user to run omnisearch as.
+            Will also control which is the WorkingDirectory
+
+            By default a user named `${default_user}` will be created.
+          '';
+        };
+        group = mkOption {
+          type = types.str;
+          default = cfg.user;
+          example = "sysSquad";
+          description = ''
+            The group to run omnisearch under.
+
+            By default a group matching user will be created.
+          '';
+        };
+
         #TODO:
         systemd = {
           enable = mkOption {
@@ -58,35 +90,45 @@
           cfg.package
         ];
 
-        #TODO: Fix with user and group
-        # A lot of this come from OG omnisearch-systemd-nix-implementation.
+        users = {
+          users.${cfg.user} = {
+            group = cfg.group;
+            home = cfg.dataDir;
+            createHome = true;
+            description = "omnisearch user";
+          };
+          groups.${cfg.group} = {
+          };
+        };
+
         systemd.services.omnisearch =
           let
             srcDir = "${pkg}/share/omnisearch";
-            WorkingDirectory = "/var/lib/omnisearch";
           in
           mkIf cfg.systemd.enable {
-            description = "Start 'omnisearch' (web meta-search engine) daemon.";
+            description = "Omnisearch Web Search Server";
             wantedBy = [ "multi-user.target" ];
             after = [ "network.target" ];
             serviceConfig = {
+              User = cfg.user;
+              Group = cfg.group;
+
               ExecStart = "${srcDir}/omnisearch";
 
-              inherit WorkingDirectory;
-              StateDirectory = "omnisearch"; # sus?
-              CacheDirectory = "omnisearch"; # sus?
+              WorkingDirectory = cfg.dataDir;
 
               BindReadOnlyPaths = [
-                "${srcDir}/templates:${WorkingDirectory}/templates"
-                "${srcDir}/static:${WorkingDirectory}/static"
-                "${srcDir}/locales:${WorkingDirectory}/locales"
-                "${srcDir}/config.ini:${WorkingDirectory}/config.ini"
+                "${srcDir}/templates:${cfg.dataDir}/templates"
+                "${srcDir}/static:${cfg.dataDir}/static"
+                "${srcDir}/locales:${cfg.dataDir}/locales"
+                "${srcDir}/config.ini:${cfg.dataDir}/config.ini"
               ];
 
               DynamicUser = true;
               ProtectSystem = "strict";
               ProtectHome = true;
               PrivateTmp = true;
+              NoNewPrivileges = true;
 
               Restart = "always";
               RestartSec = 5;
